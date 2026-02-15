b565aa9fca513d2e35d79a9576311b8ed69368f0
Shakil <shakilkabir.h@gmail.com>
2026-02-15 11:25:47 +0600

    Add files via upload

---
+++ JavaScript/DOM.js
@@ -0,0 +1,21 @@
+// const element = document.getElementById('demo');
+
+// console.log(element.innerText); // only visible text
+
+// console.log(element.textContent); // visible and hidden text
+
+// console.log(element.innerHTML) // all things, even tag
+
+
+// const title = document.getElementById('main-title');
+// title.style.color = 'blue';
+
+// const paragraphs = document.getElementsByClassName('text')
+// paragraphs[0].style.fontWeight = 'bold';
+
+// const firstPara = document.querySelector(".text");
+// const sameTitle = document.querySelector("#main-title")
+// console.log(sameTitle.innerText)
+
+const box = document.getElementById('demo');
+box.innerHTML = "<b>Hello</b>";
