9a445c0f87b6abb3d4c4aac8877bc0f32d5a4a90
Shakil <shakilkabir.h@gmail.com>
2026-02-17 23:09:10 +0600

    Add files via upload

---
+++ JavaScript/Random-User/user.js
@@ -0,0 +1,26 @@
+const nameShow = document.getElementById("name");
+const photoShow = document.getElementById("photo");
+const emailShow = document.getElementById("email");
+const loadBtn = document.getElementById("btn");
+
+async function fetchData() {
+    try {
+        const response = await fetch('https://randomuser.me/api/');
+        const data = await response.json();
+
+        // using Destructuring extract data
+        const user = data.results[0];
+        const {first, last} = user.name;
+        const email = user.email;
+        const photo = user.picture.large;
+
+        nameShow.textContent = `${first} ${last}`;
+        emailShow.textContent = email;
+        photoShow.src = photo;
+    }   catch (error) {
+        console.log("File not found or error", error)
+    }
+}
+
+loadBtn.addEventListener('click', fetchData);
+fetchData();
