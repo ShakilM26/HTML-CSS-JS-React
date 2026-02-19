9ed313591872ae396f2dc83689c65f1726a06525
Shakil <shakilkabir.h@gmail.com>
2026-02-19 10:53:28 +0600

    Add files via upload

--- JavaScript/counter app/scripts.js
+++ JavaScript/counter app/scripts.js
@@ -10,12 +10,8 @@ function increase() {
 }
 // decrease func
 function decrease() {
-    if (count > 0) {
         count = count - 1;
         document.getElementById("count").innerText = count;
-    } else {
-        count = 0;
-       }
 }
 // reset function
 function reset() {
